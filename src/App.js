da09ae457976c1b7bba768aa46c1e8b23d5f07d4
yaswanth <yaswanthrangu789@gmail.com>
2023-10-20 17:47:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+// import Button from 'react-bootstrap/Button';
+// import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { Color } from './components/colorpicker.js';
+
+function App() {
+  return (
+    <Color/>
+  );
+}
+
+export default App;
